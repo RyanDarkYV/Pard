0e5fce5cd6d4f9efb1a86a0122795484cf35e537
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-11-02 17:06:46 +0200

    Added interfaces for command/query & handlers

---
+++ Pard.Application/common/Abstractions/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Pard.Application.Common.Abstractions.Commands
+{
+    public interface ICommand : IRequest
+    {
+
+    }
+
+    public interface ICommand<out TResult> : IRequest<TResult>
+    {
+
+    }
+}
---
+++ Pard.Application/common/Abstractions/Queries/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Pard.Application.Common.Abstractions.Queries
+{
+    public interface IQueryHandler<in TQuery, TResult> : IRequestHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        
+    }
+}
