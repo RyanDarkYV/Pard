d9b565fd357c656fdd7bc0a5026fc74a9ba9d5c0
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-11-03 23:53:47 +0200

    Added mock for a notification service.

---
+++ Pard.Infrastructure/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Pard.Application.Common.Interfaces;
+using Pard.Application.Models;
+
+namespace Pard.Infrastructure
+{
+    public class NotificationService : INotificationService
+    {
+        public Task SendAsync(MessageDto message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
